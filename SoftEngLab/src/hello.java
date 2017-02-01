4f97093d42c89c5830c1a264b73f187f276339c8
orlandoc096 <orlandoc096@cis17.strose.edu>
2017-02-01 11:53:24 -0500

    Hi

---
+++ SoftEngLab/src/hello.java
@@ -0,0 +1,17 @@
+
+//This is the Hello java file
+//Second part of lab two
+
+
+public class Hello {
+
+    public static void main(String args[]) {
+
+	if(args[].length > 0) {
+	    
+	System.out.println("Hello " + args[0]);
+	System.out.println("This is a collaborator!");
+
+	}
+
+}
